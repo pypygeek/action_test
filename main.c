4ed123080755d4566d1bec861c93fe3fc5bcfb33
서벨롭 <pypygeek@gmail.com>
2022-10-29 20:33:42 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ *	MSP C언어 프로그램 - 자료 구조 과제(List Program)
+ *
+ *	입력으로 주어지는 길이만큼의 실수형 데이터를 저장하는 Linked list data structure 생성
+ *	List에 새로운 데이터를 inser, append하는 함수, 임의의 노드를 delete하는 함수 프로그램 작성
+ *	Single linked list, doubly linked list로 각각 구현
+ *
+ *	서민원 (20202170)
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello, goorm!\n");
+	
+	return 0;
+}
